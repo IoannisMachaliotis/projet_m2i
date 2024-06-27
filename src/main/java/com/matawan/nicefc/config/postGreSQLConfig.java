7f86a7a4af6e54edef21d6d944e375d2f47c5814
Administrator <gitlab_admin_939b49@example.com>
2024-06-27 11:49:04 +0200

    Added docker deploy configuration locally

--- src/main/java/com/matawan/nicefc/config/postGreSQLConfig.java
+++ src/main/java/com/matawan/nicefc/config/postGreSQLConfig.java
@@ -16,24 +16,24 @@ import javax.sql.DataSource;
 public class postGreSQLConfig {
     private static final Logger logger = LoggerFactory.getLogger(postGreSQLConfig.class);
 
-    /**
-     * Configures and provides an embedded PostGreSQL DataSource.
-     *
-     * @return The configured embedded PostGreSQL DataSource.
-     * @throws Exception If an error occurs while starting the embedded PostGreSQL instance.
-     */
-    @Bean
-    @Primary
-    public DataSource embeddedPostGreSqlDb() throws Exception {
 
-        // Start an embedded PostGreSQL instance using the EmbeddedPostgres builder
-        // and retrieve the corresponding DataSource.
-        DataSource embeddedPostGreDS = EmbeddedPostgres.builder().start().getPostgresDatabase();
+    // /**
+    //  * Configures and provides an embedded PostGreSQL DataSource.
+    //  *
+    //  * @return The configured embedded PostGreSQL DataSource.
+    //  * @throws Exception If an error occurs while starting the embedded PostGreSQL instance.
+    //  */
+    // @Bean
+    // @Primary
+    // public DataSource embeddedPostGreSqlDb() throws Exception {
 
-        logger.info("Embedded PostGreSQL DataSource configured successfully.");
+    //     // Start an embedded PostGreSQL instance using the EmbeddedPostgres builder
+    //     // and retrieve the corresponding DataSource.
+    //     DataSource embeddedPostGreDS = EmbeddedPostgres.builder().start().getPostgresDatabase();
 
-        return embeddedPostGreDS;
-    }
+    //     logger.info("Embedded PostGreSQL DataSource configured successfully.");
 
+    //     return embeddedPostGreDS;
+    // }
 
 }
