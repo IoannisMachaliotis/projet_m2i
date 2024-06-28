29f91107d55c407cde37cc569de0861701721603
Administrator <gitlab_admin_939b49@example.com>
2024-06-28 10:06:58 +0200

    Added ssh key

--- main.tf
+++ main.tf
@@ -106,6 +106,6 @@ resource "azurerm_linux_virtual_machine" "main" {
   computer_name = "hostname"
   admin_ssh_key {
     username   = "testadmin"
-    public_key = file("~/.ssh/authorized_keys")
+    public_key = file("/home/ubuntu-unix/.ssh/id_rsa.pub")
   }
 }
